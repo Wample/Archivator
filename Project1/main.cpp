d4fdf3c7b3bc1da9f4698fde7121818508bcae17
ddllab <ddl.rybchenko@gmail.com>
2021-04-24 13:23:40 +0300

    *

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,5 +1,18 @@
 #include <iostream>
 
+//create class iArchivator with method Compress/Decompress(pure  virtual function)
+//create class eRLEArchivator which inheritence of iArchivator and archived plain text
+//code in main for exmaple
+/*
+* string text = "London is the capiiiital of GB";
+* eRLEArchivator archivator;
+* string compressedText;
+* archivator.Compress(text, compressedText);
+* cout<<compressedText<<endl; //must printed L1o1n1d1... i4t1...
+* string decompressedText;
+* archivator.DeCompress(compressedText, decompressedText);
+* cout<<decompressedText<<endl; //must printed London is the capiiiital of GB.
+*/
 int main()
 {
 
